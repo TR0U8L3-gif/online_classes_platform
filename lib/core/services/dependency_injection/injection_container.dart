4a4784e7128608c049113f6438c79e80d748bdbd
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2024-01-06 19:51:18 +0100

    project setup

--- lib/core/services/dependency_injection/injection_container.dart
+++ lib/core/services/dependency_injection/injection_container.dart
@@ -1,39 +1,29 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
-import '../../../core/services/network/network_info.dart';
-import '../../../src/authentication/data/data_sources/auth_remote_data_source.dart';
-import '../../../src/authentication/data/repositories/auth_repository_impl.dart';
-import '../../../src/authentication/domain/repositories/auth_repository.dart';
-import '../../../src/authentication/domain/usecases/create_user.dart';
-import '../../../src/authentication/domain/usecases/get_user.dart';
-import '../../../src/authentication/domain/usecases/get_users.dart';
-import '../../../src/authentication/presentation/cubit/auth_cubit.dart';
+import 'package:online_classes_platform/core/services/network/network_info.dart';
 
+
+/// GetIt Service Locator Instance
 final sl = GetIt.instance;
 
+/// Asynchronous function setting up dependency injection
 Future<void> setupServiceLocator() async {
-
-  //Src - authentication feature
+  //Src
   //Cubit
-  sl.registerFactory(() => AuthCubit(getUser: sl(), getUsers: sl(), createUser: sl()));
   //UseCase
-  sl.registerLazySingleton(() => CreateUser(sl()));
-  sl.registerLazySingleton(() => GetUser(sl()));
-  sl.registerLazySingleton(() => GetUsers(sl()));
   //Repository
-  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(remoteDataSource: sl(), networkInfo: sl()));
   //DataSource
-  sl.registerLazySingleton<AuthRemoteDataSource>(() => AuthRemoteDataSourceImpl(dio: sl()));
 
   //Services
   //NetworkInfo
-  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(internetConnectionChecker: sl()));
+  sl.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(internetConnectionChecker: sl()),
+    );
 
   //External
   //Dio
-  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(Dio.new);
   //Internet connection checker
   sl.registerSingleton(InternetConnectionChecker());
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'core/services/dependency_injection/injection_container.dart';
-import 'src/authentication/presentation/cubit/auth_cubit.dart';
-import 'src/authentication/presentation/pages/home_screen.dart';
+import 'package:online_classes_platform/core/services/dependency_injection/injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,20 +7,20 @@ void main() async {
   runApp(const MyApp());
 }
 
+/// Widget representing the main application
 class MyApp extends StatelessWidget {
+
+  /// Constructor with optional 'key' parameter for MyApp.
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => sl<AuthCubit>(),
-      child: MaterialApp(
-        theme: ThemeData(
-          useMaterial3: true,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: const HomeScreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: const Scaffold(),
     );
   }
 }
